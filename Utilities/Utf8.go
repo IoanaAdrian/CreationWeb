f32300469a9abd20e29f45bc552ada8bf6f340e2
Ioana Adrian <adrian212005@yahoo.ro>
2021-06-30 01:06:37 +0300

    File system done :alien:

---
+++ Utilities/Utf8.go
@@ -0,0 +1,60 @@
+package Utilities
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Convert(path string) string {
+	var result string
+	//var windowsResult string
+	for i := 0; i < len(path); i++ {
+		if path[i] == '%' {
+			result += mapValues(path[i+1 : i+3])
+			i += 2
+		} else {
+			result += string(path[i])
+		}
+		//windowsResult += string(result[i])
+		//if result[i] == ' ' {
+		//	windowsResult += "` "
+		//} else {
+		//	windowsResult += string(result[i])
+		//}
+	}
+	return result
+}
+
+func mapValues(val string) string {
+	fmt.Println(val)
+	var intVal, _ = strconv.Atoi(val)
+	switch intVal {
+	case 20:
+		return " "
+	case 21:
+		return "!"
+	case 22:
+		return "\""
+	case 23:
+		return "#"
+	case 24:
+		return "$"
+	case 25:
+		return "%"
+	case 26:
+		return "&"
+	case 27:
+		return "'"
+	case 28:
+		return "("
+	case 29:
+		return ")"
+	}
+	return ""
+}
+
+func main() {
+
+	fmt.Println(Convert("/testdir/txt1%20-%20Copy%20%283%29.txt"))
+
+}
--- main.go
+++ main.go
@@ -1,34 +1,65 @@
 package main
-//
-//import (
-//	"fmt"
-//	"log"
-//	"net/http"
-//	"io/ioutil"
-//)
-//
-//var PORT = ":8080"
-//
-//func checkError(e error) {
-//	if e != nil {
-//		panic(e)
-//	}
-//}
-//
-//func handler(w http.ResponseWriter, r *http.Request) {
-//	if r.URL.Path[1:] == "" {
-//		_, err := fmt.Fprintf(w, "Hi there, I love %s !", r.URL.Path[1:])
-//		checkError(err)
-//	}
-//}
-//
-//func viewHandler(w http.ResponseWriter, r *http.Request) {
-//	title := r.URL.Path[len("/view/"):]
-//	p, _ := loadPage(title)
-//	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-//}
-//
-//func main() {
-//	http.HandleFunc("/", handler)
-//	log.Fatal(http.ListenAndServe(PORT, nil))
-//}
+
+import (
+	ut "./Utilities"
+	"bytes"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type tmp struct {
+	Name string
+	Path string
+}
+
+//var displayPath = "./testdir"
+
+func main() {
+
+	http.HandleFunc("/", handler)
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ip,err := ut.GetClientIp(r)
+	if err!=nil{
+		panic(err)
+	}
+	fmt.Println(ip)
+	if strings.HasPrefix(r.URL.String(), "/testdir") {
+		var path = "." + r.URL.String()
+		windowsPath := ut.Convert(path)
+		fmt.Println(windowsPath)
+		//var windowsPath = strings.Replace(path, "%20", "` ", -1)
+		instance, err := os.Stat(windowsPath)
+		if err != nil {
+			panic(err)
+		}
+		if instance.Mode().IsDir() {
+			file, err := ioutil.ReadDir(windowsPath)
+			if err != nil {
+				panic(err)
+			}
+			for _, f := range file {
+				t := template.New("ex")
+				p := tmp{f.Name(), windowsPath[1:]}
+				t, _ = t.Parse("<a href='{{.Path}}/{{.Name}}'>{{.Name}}</a>")
+				_ = t.Execute(w, p)
+			}
+		} else {
+			content, err := ioutil.ReadFile(windowsPath)
+			if err != nil {
+				panic(err)
+			}
+			w.Header().Set("Content-Disposition", "attachment; filename="+instance.Name())
+			w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+			_, _ = io.Copy(w, bytes.NewReader(content))
+		}
+	}
+
+}
